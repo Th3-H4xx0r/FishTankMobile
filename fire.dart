18cd6935fc449a9561f7918842466277929cd54b
Holland Pleskac <56204301+HollandPleskac@users.noreply.github.com>
2020-05-02 13:40:50 -0700

    Update fire.dart

--- fire.dart
+++ fire.dart
@@ -5,14 +5,27 @@ final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
 final Firestore _firestore = Firestore.instance;
 
 class Fire {
+  createAccount(
+    String email,
+    String uid,
+    String displayName,
+  ) {
+    _firestore.collection('UserData').document(uid).setData(
+      {
+        "email": email,
+        'profile image url': '',
+        'display name': displayName,
+      },
+    );
+  }
 
   uploadProfileImageToDatabase(imageUrl) async {
     // only one image because this a profile picture so no need to create a new collection
     final user = await _firebaseAuth.currentUser();
-    _firestore
-        .collection("UserData")
-        .document(user.uid)
-        .updateData({'profile image url': imageUrl});
+    _firestore.collection("UserData").document(user.uid).updateData(
+      {
+        'profile image url': imageUrl,
+      },
+    );
   }
-
 }
